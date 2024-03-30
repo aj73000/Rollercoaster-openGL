b163a2c805e3f4db5d3ce2c10ca9a8a3a272651b
Arthur <47077096+aj73000@users.noreply.github.com>
2024-03-30 02:22:38 +0000

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/Rollercoaster-openGL.dir/Physics/PhysicsCollision.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Rollercoaster-openGL.dir/Physics/PhysicsCollision.cpp.o: \
- /home/arthur/Documents/Cpp/Rollercoaster-openGL/Physics/PhysicsCollision.cpp \
- /usr/include/stdc-predef.h \
- /home/arthur/Documents/Cpp/Rollercoaster-openGL/Physics/PhysicsCollision.h
